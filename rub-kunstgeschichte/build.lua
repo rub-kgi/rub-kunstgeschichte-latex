4b87496e8379d983e835a0eb776c4aedef6c5e6a
Joran Schneyer <19168395+BizarrePenguin@users.noreply.github.com>
2024-07-25 11:15:18 +0000

    Add automated tagging function in build.lua
    to automatically tag the dtx file with a version and date

--- rub-kunstgeschichte/build.lua
+++ rub-kunstgeschichte/build.lua
@@ -6,3 +6,20 @@ module = "rub-kunstgeschichte"
 
 -- Typeset example files
 typesetfiles = { "*.dtx", "*example.tex" }
+
+-- Automated tagging
+function update_tag(file,content,tagname,tagdate)
+    tagname = string.gsub(tagname, "^v", "")
+
+    if string.match(file,"rub%-kunstgeschichte.dtx") then
+        -- rub-kunstgeschichte.dtx"
+        content = string.gsub(content,
+            "\\ProvidesClass%{rub%-kunstgeschichte%}\n.+\n    %[%d%d%d%d%-%d%d%-%d%d v%d+%.%d+%.%d+",
+            "\\ProvidesClass{rub-kunstgeschichte}\n%%<*class>\n    [" .. tagdate .. " v" .. tagname)
+        content = string.gsub(content,
+            "\\changes%{unreleased%}%{unreleased%}",
+            "\\changes{v" .. tagname .. "}{" .. tagdate .. "}")
+        return content
+    end
+    return content
+end
