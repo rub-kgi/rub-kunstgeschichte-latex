a203c83191948810e549e613ad97d4a7753ef07f
Joran Schneyer <19168395+BizarrePenguin@users.noreply.github.com>
2024-09-06 13:37:17 +0200

    Add automated CTAN upload
    Closes #26

--- rub-kunstgeschichte/build.lua
+++ rub-kunstgeschichte/build.lua
@@ -7,6 +7,20 @@ module = "rub-kunstgeschichte"
 -- Typeset example files
 typesetfiles = { "*.dtx", "*example.tex" }
 
+-- CTAN Upload
+uploadconfig = {
+    pkg = "rub-kunstgeschichte",
+    author = "Joran Schneyer",
+    uploader = "Joran Schneyer",
+    license = "lppl1.3c",
+    summary = "A class for the art history institute at Ruhr University Bochum",
+    ctanPath = "/macros/latex/contrib/rub-kunstgeschichte",
+    repository = "https://github.com/rub-kgi/rub-kunstgeschichte-latex",
+    bugtracker = "https://github.com/rub-kgi/rub-kunstgeschichte-latex/issues",
+    update = true,
+    note = [[Uploaded automatically by l3build...]]
+}
+
 -- Automated tagging
 function update_tag(file,content,tagname,tagdate)
     tagname = string.gsub(tagname, "^v", "")
